0afa4aea0eeecc85560db4f9237dc437b9e7d5a7
burhanmorningstar <222802042@ogr.cbu.edu.tr>
2024-05-11 20:33:40 +0300

    login register resetpassword islemleri tamamlandi

--- website/script/signup.js
+++ website/script/signup.js
@@ -1,14 +1,21 @@
 const apiUrl = "http://localhost:3000";
-const registerSubmitBtn = document.getElementById("registerSubmitBtn");
+const registerForm = document.getElementById("registerForm");
 
-registerSubmitBtn.addEventListener("click", async function (event) {
+registerForm.addEventListener('submit', async (event) =>{
   event.preventDefault();
 
   const email = document.getElementById("user_email_address").value;
   const password = document.getElementById("user_password").value;
+  const confirmPassword = document.getElementById("confirm_password").value;
   const fullname = document.getElementById("user_fullname").value;
   
   try {
+    if (!email || !password || !confirmPassword || !fullname) {
+      throw new Error('Lütfen tüm alanları doldurunuz.');
+  }
+    if (password !== confirmPassword) {
+      throw new Error('Girilen parolalar uyuşmuyor.');
+    }
     const response = await fetch(apiUrl + "/register", {
       method: "POST",
       headers: {
@@ -16,25 +23,14 @@ registerSubmitBtn.addEventListener("click", async function (event) {
       },
       body: JSON.stringify({
         user_email_address: email,
-        user_password: password,
+        user_password: confirmPassword,
         user_fullname: fullname,
       }),
     });
 
     const data = await response.json();
-    if (response.ok) {
-      // Giriş başarılı
-      console.log("Kayit başarılı.");
-
-      // Kullanıcıyı yönlendir (örneğin ana sayfaya)
-      //window.location.href = "/index.html";
-    } else {
-      // Giriş başarısız, hata mesajını göster
-      console.error(data.message);
-      alert(data.message); // Kullanıcıya göstermek için bir uyarı penceresi aç
-    }
+    alert(data.message);
   } catch (error) {
-    //console.error("Bir hata oluştu:", error);
-    alert("Bir hata oluştu. Lütfen tekrar deneyin.");
+    console.error("Bir hata oluştu:", error);
   }
 });
