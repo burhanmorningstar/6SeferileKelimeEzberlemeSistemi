3c58fccfbe7a5e554ec0b56ca6d1ea8f83540e03
Aybavs <222803012@ogr.cbu.edu.tr>
2024-05-17 10:29:13 +0300

    css
    quiz ekranı şekillendi

--- website/script/quiz.js
+++ website/script/quiz.js
@@ -1,55 +1,46 @@
 const wordApiUrl = "http://localhost:3001";
 const userApiUrl = "http://localhost:3000";
 
-// Kullanıcı cevaplarını saklayacak diziler
+const answerInput = document.getElementById("answer");
+
 let correctAnswers = [];
 let userAnswers = [];
-let resultQuestions = [];
 let questions = [];
 let currentQuestionIndex = 0;
 let userId;
 
-// Event listener'ları güncelle
 document.addEventListener("DOMContentLoaded", () => {
-  // Kullanıcı kimliğiyle soruları getir
   const urlParams = new URLSearchParams(window.location.search);
   userId = urlParams.get("user_id");
   fetchQuestions(userId);
 });
 
-/// fetchQuestions fonksiyonunu güncelle
 function fetchQuestions(userId) {
   fetch(wordApiUrl + "/questions/" + userId)
     .then((response) => response.json())
     .then((data) => {
       questions = data;
       console.log("Sorular başarıyla getirildi:", questions);
-      // Doğru ve kullanıcı cevaplarını sıfırla
       correctAnswers = [];
       userAnswers = [];
       closeModal();
-      showNextQuestion(); // fetch işlemi tamamlandığında ilk soruyu göster
+      showNextQuestion();
     })
     .catch((error) =>
       console.error("Kelime alınırken bir hata oluştu:", error)
     );
 }
 
-function submitAnswer() {
-  const answerInput = document.getElementById("answer");
-  const answer = answerInput.value.trim(); // Trim whitespace
+function submitAnswer(wordId, userId) {
+  const answer = answerInput.value.trim();
   if (!answer) {
     alert("Answer can't be empty.");
     return;
   }
 
-  const currentQuestion = questions[currentQuestionIndex - 1];
-  const wordId = currentQuestion.word_id;
-  const userId = currentQuestion.user_id;
-
   submitAnswerToApi(userId, wordId, answer);
 }
-// submitAnswerToApi fonksiyonunu güncelle
+
 async function submitAnswerToApi(userId, wordId, answer) {
   try {
     const response = await fetch(wordApiUrl + "/answer", {
@@ -65,13 +56,11 @@ async function submitAnswerToApi(userId, wordId, answer) {
     });
     const result = await response.json();
 
-    // Doğru veya yanlış cevaba göre işlem yap
     result.message === "Correct answer!"
       ? console.log("Correct answer!")
       : console.log("Incorrect answer.");
-    userAnswers.push(answer); // Kullanıcının cevabını ekleyin
+    userAnswers.push(answer);
 
-    // Sonraki soruyu göster
     showNextQuestion();
   } catch (error) {
     console.error("Cevap gönderilirken bir hata oluştu:", error);
@@ -93,67 +82,34 @@ function showNextQuestion() {
 }
 
 const updateHtml = (question) => {
-  const questionContainer = document.getElementById("question-container");
   if (question) {
-    // Yeni bir div öğesi oluştur
-    const newQuestionDiv = document.createElement("div");
+    const englishParagraph = document.getElementById("questionWord");
+    englishParagraph.textContent = `${question.word}`;
 
-    // İngilizce kelimeyi ve cümleyi ekle
-    const englishParagraph = document.createElement("p");
-    englishParagraph.textContent = `English: ${question.word}`;
-    newQuestionDiv.appendChild(englishParagraph);
+    const usageParagraph = document.getElementById("questionSentence");
+    usageParagraph.textContent = `${question.word_in_sentence}`;
 
-    const usageParagraph = document.createElement("p");
-    usageParagraph.textContent = `Sentence: ${question.word_in_sentence}`;
-    newQuestionDiv.appendChild(usageParagraph);
-
-    // Resim ve ses öğelerini ekleyin
-    const imageElement = document.createElement("img");
+    const imageElement = document.getElementById("wordImg");
     imageElement.src = question.word_image;
-    imageElement.alt = "Image";
-    newQuestionDiv.appendChild(imageElement);
 
-    const audioElement = document.createElement("audio");
-    audioElement.controls = true;
-    const audioSource = document.createElement("source");
-    audioSource.src = question.word_voice;
-    audioSource.type = "audio/mpeg"; // MP3 dosyaları için tip
-    audioElement.appendChild(audioSource);
-    newQuestionDiv.appendChild(audioElement);
+    const audioElement = document.getElementById("wordAudio");
+    audioElement.src = question.word_voice;
 
-    // Cevap giriş alanını ve düğmesini ekleyin
-    const answerInput = document.createElement("input");
-    answerInput.id = "answer";
-    answerInput.type = "text";
-    answerInput.placeholder = "Enter your answer";
-    newQuestionDiv.appendChild(answerInput);
-
-    const submitButton = document.createElement("button");
-    submitButton.id = "submitBtn";
-    submitButton.textContent = "Submit";
+    const submitButton = document.getElementById("submitBtn");
     submitButton.addEventListener("click", () =>
       submitAnswer(question.word_id, question.user_id)
     );
-    newQuestionDiv.appendChild(submitButton);
 
-    // Listen for "Enter" key press event
     answerInput.addEventListener("keydown", (event) => {
       if (event.key === "Enter") {
         submitAnswer(question.word_id, question.user_id);
       }
     });
-
-    // Önceki soruyu temizle ve yeni soruyu ekle
-    questionContainer.innerHTML = "";
-    questionContainer.appendChild(newQuestionDiv);
   } else {
-    questionContainer.innerHTML = "";
-    questionContainer.innerHTML = "<p>No more questions available.</p>";
     const resetButton = document.createElement("button");
     resetButton.textContent = "Reset Quiz";
     resetButton.id = "resetBtn";
     resetButton.addEventListener("click", resetQuiz);
-    questionContainer.appendChild(resetButton);
     showResults();
   }
 };
@@ -161,13 +117,13 @@ const updateHtml = (question) => {
 function resetQuiz() {
   correctAnswers = [];
   userAnswers = [];
-  resultQuestions = [];
   questions = [];
   currentQuestionIndex = 0;
   fetchQuestions(userId);
 }
 
 function closeModal() {
+  const modal = document.getElementById("resultModal");
   modal.classList.remove("show");
   modal.style.display = "none";
 }
@@ -177,11 +133,11 @@ var tbody = document.getElementById("cevaplar-tablosu");
 function showResults() {
   tbody.innerHTML = "";
 
-  for (var i = 0; i < resultQuestions.length; i++) {
+  for (var i = 0; i < questions.length; i++) {
     var tr = document.createElement("tr");
 
     var tdSoru = document.createElement("td");
-    tdSoru.textContent = resultQuestions[i];
+    tdSoru.textContent = questions[i].word;
     tr.appendChild(tdSoru);
 
     var tdKullaniciCevabi = document.createElement("td");
@@ -190,11 +146,11 @@ function showResults() {
     tr.appendChild(tdKullaniciCevabi);
 
     var tdDogruCevap = document.createElement("td");
-    tdDogruCevap.textContent = correctAnswers[i];
+    tdDogruCevap.textContent = questions[i].correct_answer;
     tr.appendChild(tdDogruCevap);
 
     var tdSonuc = document.createElement("td");
-    var questionCheck = checkAnswer(userAnswers[i], correctAnswers[i]);
+    var questionCheck = checkAnswer(userAnswers[i], questions[i].correct_answer);
     tdSonuc.textContent = questionCheck === 1 ? "Doğru" : "Yanlış";
 
     tr.appendChild(tdSonuc);
@@ -204,16 +160,5 @@ function showResults() {
 }
 
 function checkAnswer(userAnswer, correctAnswer) {
-  if (userAnswer === correctAnswer) {
-    return 1;
-  } else {
-    return 0;
-  }
+  return userAnswer === correctAnswer ? 1 : 0;
 }
-
-document.getElementById("closeModalBtn").addEventListener("click", function () {
-  closeModal();
-  resetQuiz();
-});
-
-var modal = document.getElementById("resultModal");
